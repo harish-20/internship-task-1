bc81143ead372ac95c144726ad8505df4f9038a5
harish kumar <harishkumarharishkumar108@gmail.com>
2022-07-31 21:14:35 +0530

    progress bar added

---
+++ src/components/Result.js
@@ -0,0 +1,30 @@
+import LinearProgress from '@mui/material/LinearProgress'
+
+import React from 'react'
+import './Result.css'
+
+const Result = ({ score, currentQuestion }) => {
+  const getWrongAns = (score, currentQuestion) => {
+    return currentQuestion - score
+  }
+
+  const normalise = (value) => value * 25
+
+  return (
+    <div className="result">
+      <div className="right-ans">
+        <span>Correct Ans</span>
+        <LinearProgress variant="determinate" value={normalise(score)} />
+      </div>
+      <div className="wrong-ans">
+        <span>Wrong Ans</span>
+        <LinearProgress
+          variant="determinate"
+          value={normalise(getWrongAns(score, currentQuestion))}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Result
